54720cf291e4d96498ff80028312bd3a2a05cd0d
Yuzong-LYZ <42740485+Yuzong-LYZ@users.noreply.github.com>
2026-06-29 12:23:37 +0800

    1. 修改项目logo：logo.png 2. 修改项目浏览器顶部图标：favicon.ico

--- yuzong-picture-frontend/src/components/GlobalHeader.vue
+++ yuzong-picture-frontend/src/components/GlobalHeader.vue
@@ -121,8 +121,8 @@ const originItems = [
   },
   {
     key: 'others',
-    label: h('a', { href: 'https://www.codefather.cn', target: '_blank' }, '编程导航'),
-    title: '编程导航',
+    label: h('a', { href: 'https://www.codefather.cn', target: '_blank' }, '关于更多'),
+    title: '关于更多',
   },
 ]
 
